cc120bae4196e5168e3a259ab09a1e58272eb2dc
Jakub K <u124_jakkie_waw@technischools.com>
2026-02-27 14:22:02 +0100

    nvm

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -57,6 +57,7 @@ export default function Checkout({ setPage }: Props) {
     };
 
     const onSubmit = (data: CheckoutData) => {
+        console.log("ORDER:", data);
         toast.success("Zamówienie złożone pomyślnie!");
         clearCart();
         setPage("home")
